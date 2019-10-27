9e8b66f67aaef7cb6b4f2278d4c3b957f102abcb
Stanley Zheng <Stanley@zheng.nyc>
2019-10-27 17:15:36 -0400

    adding hashing and deep links

--- firehose.py
+++ firehose.py
@@ -1,5 +1,8 @@
 from __future__ import print_function
 import os
+import re
+import zulip
+
 
 from dotenv import load_dotenv
 
@@ -7,34 +10,25 @@ load_dotenv()
 
 from typing import Any, Dict
 
-usage = """print-messages [options]
-Prints out each message received by the indicated bot or user.
-Example: print-messages
-Specify your Zulip API credentials and server in a ~/.zuliprc file or using the options.
-"""
-
-import zulip
-
 ZULIP_EMAIL = os.getenv("ZULIP_EMAIL")
 ZULIP_KEY = os.getenv("ZULIP_KEY")
 ZULIP_SITE = os.getenv("ZULIP_SITE")
 
-# parser = zulip.add_default_arguments(argparse.ArgumentParser(usage=usage))
-# options = parser.parse_args()
+# TO_STREAM = "firehose"
+TO_STREAM = "test-stream"
 
 client = zulip.Client(email=ZULIP_EMAIL, api_key=ZULIP_KEY, site=ZULIP_SITE)
-# client = zulip.Client(config_file="./zuliprc")
+
+
 last_sent = {}
+
 
 def print_message(message):
     # type: (Dict[str, Any]) -> None
     print(message)
     send_message(message)
 
-import re
-
-
-def narrow_link(REALM_STREAM, stream_id, stream_name, topic):
+def narrow_link(REALM_STREAM, stream_id, stream_name, topic, near):
     """
     https://recurse.zulipchat.com/#narrow/stream/102312-consciousness/topic/Stanley.20Zheng
     {REALM_STREAM}/{stream-id}-{stream-name}/topic/{topic}
@@ -42,46 +36,55 @@ def narrow_link(REALM_STREAM, stream_id, stream_name, topic):
     REALM_STREAM = "https://recurse.zulipchat.com/#narrow"
     stream_name = re.sub(r"\W+", "-", stream_name)
     topic = topic
-    url = "{REALM_STREAM}/stream/{stream_id}-{stream_name}/topic/{topic}".format(
+    url = "{REALM_STREAM}/stream/{stream_id}-{stream_name}/topic/{topic}/near/{near}".format(
         REALM_STREAM=REALM_STREAM,
         stream_id=stream_id,
         stream_name=stream_name,
         topic=topic,
+        near=near
     )
     return url
 
 
 def send_message(message):
-    TO_STREAM = "test-stream"
+    """
+    Send messages to test stream
+    """
+
     PRIVATE = message.get("type") == "private"
     FROM_STREAM = message.get("display_recipient")
+    stream_topic_hash = "{display_recipient}>{subject}"
 
     narrow = narrow_link(
-        "", message.get("stream_id"), FROM_STREAM, message.get("subject")
+        "", message.get("stream_id"), FROM_STREAM, message.get("subject"), message.get('id')
     )
 
-    content = "in topic:{subject} from {sender_full_name}. [link]({narrow}) ".format(
-        narrow=narrow, **message
+    content = "In topic: #**{subject}** from **{sender_full_name}**. #**{display_recipient}>{subject}**. [deeplink]({narrow}) ".format(narrow=narrow,
+        **message
     )
-    if last_sent.get(content):
+
+    content2 = " #**{display_recipient}>{subject}**.  from **{sender_full_name}**. [deeplink]({narrow}) ".format(narrow=narrow,
+        **message
+    )
+
+    if last_sent.get(stream_topic_hash):
         return
 
     request = {
         "type": "stream",
-        "to": "test-stream",
+        "to": TO_STREAM,
         "subject": FROM_STREAM,
-        "content": content,
+        "content": content2,
     }
+
     if TO_STREAM != FROM_STREAM and not PRIVATE:
 
         client.send_message(request)
-        last_sent[content] = True
+        last_sent[stream_topic_hash] = True
 
 
 # This is a blocking call, and will continuously poll for new messages
 
 if __name__ == "__main__":
 
-    client.call_on_each_message(print_message)      
-
-# https://recurse.zulipchat.com/#narrow/stream/101477-virtual-check-in/topic/2019-10-27
+    client.call_on_each_message(print_message)
